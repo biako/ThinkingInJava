f3d14858403b3672b32134e814017941bd8b4636
biako <ryushoryu@163.com>
2016-12-20 22:21:41 +0800

    Initial

--- src/com/thinkinginjava/Main.java
+++ src/com/thinkinginjava/Main.java
@@ -14,6 +14,7 @@ public class Main {
         a.method1("Value from Main");*/
         InheritanceInitialization.initialize();
         UpDownCastingTest.run();
+        UpDownCastingTest.run();
 
     }
 }
