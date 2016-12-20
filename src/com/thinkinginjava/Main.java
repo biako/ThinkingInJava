ce6574e2ef15593f9aead9acf7ab0b24133df490
biako <ryushoryu@163.com>
2016-12-20 22:18:19 +0800

    Initial

---
+++ src/com/thinkinginjava/Main.java
@@ -0,0 +1,19 @@
+package com.thinkinginjava;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*//Call static methods in the class.
+        HelloWorld.method1(1);
+        HelloWorld.method1("string");
+        HelloWorld.method2("abc", "def", "ghi", "jkl");
+        HelloWorld.method2();*/
+        /*HelloWorld2 a = new HelloWorld2();
+        a.method1("Value from Main");*/
+        InheritanceInitialization.initialize();
+        UpDownCastingTest.run();
+
+    }
+}
