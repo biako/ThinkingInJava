ac5a91e6da11e0db6002c934cd9dc1dff4f4dd67
biako <ryushoryu@163.com>
2017-01-01 21:54:13 +0800

    Initial

--- src/tij/generics/wildcards/GenericReading.java
+++ src/tij/generics/wildcards/GenericReading.java
@@ -19,6 +19,21 @@ public class GenericReading {
 
 
     // A static method adapts to each call:
+    /**
+     * the first method readExact( ) uses the precise type.
+     * So if you use the precise type with no wildcards,
+     * you can both write and read that precise type into
+     * and out of a List.
+     *
+     * In addition, for the return value,
+     * the static generic method readExact( ) effectively "adapts"
+     * to each method call, and returns an Apple from a List<Apple>
+     * and a Fruit from a List<Fruit>, as you can see in f1( ).
+     *
+     * Thus, if you can get away with a static generic method,
+     * you don't necessarily need covariance if you're just reading.
+     *
+     * */
     static void f1() {
         Apple a = readExact(apples);
         Fruit f = readExact(fruit);
@@ -26,20 +41,38 @@ public class GenericReading {
     }
 
     // If, however, you have a class, then its type is
-    // established when the class is instantiated:
+    // established when the class is instantiated:.
+    /**
+     * If you have a generic class, however, the parameter is established
+     * for the class when you make an instance of that class. As you can
+     * see in f2( ), the fruitReader instance can read a piece of Fruit
+     * from a List<Fruit>, since that is its exact type. But a List<Apple>
+     * should also produce Fruit objects, and the fruitReader doesn't allow this.
+     *
+     * */
     static class Reader<T> {
         T readExact(List<T> list) { return list.get(0); }
     }
 
     static void f2() {
-        Reader<Fruit> fruitReader = new Reader<Fruit>();
+        Reader<Fruit> fruitReader = new Reader<>();
         Fruit f = fruitReader.readExact(fruit);
-    // Fruit a = fruitReader.readExact(apples); // Error:
-    // readExact(List<Fruit>) cannot be
-    // applied to (List<Apple>).
+        // Fruit a = fruitReader.readExact(apples); // Error:
+        // readExact(List<Fruit>) cannot be applied to (List<Apple>).
     }
 
-        static class CovariantReader<T> {
+
+
+    /**
+     * To fix the problem, the CovariantReader.readCovariant( ) method
+     * takes a List<? extends T>, and so it's safe to read a T from that
+     * list (you know that everything in that list is at least a T, and
+     * possibly something derived from a T). In f3( ) you can see that
+     * it's now possible to read a Fruit from a List<Apple>.     *
+     *
+     * */
+
+    static class CovariantReader<T> {
         T readCovariant(List<? extends T> list) {
             return list.get(0);
         }
@@ -47,7 +80,7 @@ public class GenericReading {
 
     static void f3() {
         CovariantReader<Fruit> fruitReader =
-                new CovariantReader<Fruit>();
+                new CovariantReader<>();
         Fruit f = fruitReader.readCovariant(fruit);
         Fruit a = fruitReader.readCovariant(apples);
     }
---
+++ src/tij/generics/wildcards/Wildcards.java
@@ -0,0 +1,10 @@
+package tij.generics.wildcards;
+
+/**
+ * Created by Xiaolong on 1/1/2017.
+ *
+ * Thinking in Java p490-491
+ *
+ */
+public class Wildcards {
+}
