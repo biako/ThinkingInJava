e8efdc4910cd9142c01f5ed67200f28318a0ac93
biako <ryushoryu@163.com>
2017-01-15 00:19:29 +0800

    Initial

--- src/tij/container2/hashing/whyhashing/hashedmap/SimpleHashMap.java
+++ src/tij/container2/hashing/whyhashing/hashedmap/SimpleHashMap.java
@@ -10,6 +10,53 @@ import java.util.*;
  * Thinking in Java p610-611
  * A demonstration hashed Map.
  *
+ * I. Bucket: The list containing key-value pairs with the same hash code for non-perfect hashed map.
+ *
+ * So the process of looking up a value starts by computing the hash code and using it
+ * to index into the array. If you could guarantee that there were no collisions (which
+ * is possible if you have a fixed number of values), then you’d have a perfect hashing
+ * junction, but that’s a special case.
+ *
+ * In all other cases, collisions are handled by external chaining: The array doesn't
+ * point directly to a value, but instead to a list of values. These values are searched
+ * in a linear fashion using the equals( ) method. Of course, this aspect of the search
+ * is much slower, but if the hash function is good, there will only be a few values in
+ * each slot. So instead of searching through the entire list, you quickly jump to a slot
+ * where you only have to compare a few entries to find the value. This is much faster,
+ * which is why the HashMap is so quick.
+ *
+ * Each of such list of value is called a bucket.
+ *
+ *
+ * II. Example of SimpleHashMap
+ *
+ * Because the "slots" in a hash table are often referred to as buckets,
+ * the array that represents the actual table is called buckets. To promote
+ * even distribution, the number of buckets is typically a prime number.
+ *
+ * Notice that it is an array of LinkedList, which automatically provides for
+ * collisions: Each new item is simply added to the end of the list in a particular
+ * bucket. Even though Java will not let you create an array of generics, it is possible
+ * to make a reference to such an array. Here, it is convenient to upcast to such an
+ * array, to prevent extra casting later in the code.
+ *
+ *
+ * Each new item is simply added to the end of the list in a particular bucket.
+ * Even though Java will not let you create an array of generics, it is possible
+ * to make a reference to such an array. Here, it is convenient to upcast to such
+ * an array, to prevent extra casting later in the code.
+ *
+ *
+
+
+
+
+ Note that this implementation is not meant to be tuned for performance; it is only
+ intended to show the operations performed by a hash map. If you look at the source
+ code for java.util.HashMap, you’ll see a tuned implementation. Also, for simplicity
+ SimpleHashMap uses the same approach to entrySet( ) as did SlowMap, which is oversimplified
+ and will not work for a general-purpose Map.
+ *
  */
 public class SimpleHashMap<K,V> extends AbstractMap<K,V> {
     // Choose a prime number for the hash table
@@ -22,13 +69,35 @@ public class SimpleHashMap<K,V> extends AbstractMap<K,V> {
     LinkedList<MapEntry<K,V>>[] buckets = new LinkedList[SIZE];
 
 
+    /**
+     * For a put( ), the hashCode( ) is called for the key and the result is forced to a
+     * positive number. To fit the resulting number into the buckets array, the modulus
+     * operator is used with the size of that array.
+     *
+     * If that location is null, it means there are no elements that hash to that
+     * location, so a new LinkedList is created to hold the object that just did hash
+     * to that location.
+     *
+
+     *
+     * */
     public V put(K key, V value) {
         V oldValue = null;
         int index = Math.abs(key.hashCode()) % SIZE;
+
+        // If that location is null, it means there are no elements that hash to that
+        // location, so a new LinkedList is created to hold the object that just did hash
+        // to that location.
         if(buckets[index] == null)
             buckets[index] = new LinkedList<MapEntry<K,V>>();
         LinkedList<MapEntry<K,V>> bucket = buckets[index];
         MapEntry<K,V> pair = new MapEntry<>(key, value);
+
+
+
+        // However, the normal process is to look through the list to see if there
+        // are duplicates, and if there are, the old value is put into oldValue and
+        // the new value replaces the old.
         boolean found = false;
         ListIterator<MapEntry<K,V>> it = bucket.listIterator();
         while(it.hasNext()) {
@@ -40,17 +109,32 @@ public class SimpleHashMap<K,V> extends AbstractMap<K,V> {
                 break;
             }
         }
+
+        // The found flag keeps track of whether an old key-value pair
+        // was found and, if not, the new pair is appended to the end of the list.
         if(!found)
             buckets[index].add(pair);
         return oldValue;
     }
 
+    /**
+     *  The get( ) calculates the index into the buckets array in the same fashion as put( )
+     * (this is important in order to guarantee that you end up in the same spot). If a
+     * LinkedList exists, it is searched for a match.
+     *
+     * */
     public V get(Object key) {
+        // First get the hash index
         int index = Math.abs(key.hashCode()) % SIZE;
+        // If the hash bucket of such index is empty, return null
         if(buckets[index] == null) return null;
+
+        // Traverse the bucket of such index and get the value if the key is found.
         for(MapEntry<K,V> iPair : buckets[index])
             if(iPair.getKey().equals(key))
                 return iPair.getValue();
+
+        // Default return null
         return null;
     }
 
